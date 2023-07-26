1b1829c35628252c586d461c7c9cdea158d6cd2d
johnisik314 <johnisik314@gmail.com>
2023-07-25 22:39:05 -0400

    json wokrs

--- AIQuestionnaire/main.py
+++ AIQuestionnaire/main.py
@@ -9,29 +9,19 @@ import json
 for i in range(4,random.randint(0,10)):
 
     p1 = Person("John",random.randint(0,70))
-    data = {
-        "name": "John",
-        "age": 30,
-        "attributes": {
-            "str": p1.str,
-            "dex": p1.dex,
-            "int": p1.int
-        }
-    }
-
-    
-    
+    data = {"name": "Kaith","age": 30,"attributes": {"str": p1.str,"dex": p1.dex,"int": p1.int}}
+    print (data)
     # Load the existing JSON data
     with open("AIQuestionnaire\data.json") as file:
-        old_data = json.load(file)
+        jfile = json.load(file)
+        print (jfile)
 
     # Append the new data to the existing JSON
-    print (data)
-    data = old_data + data
-    print (data)
-    # Write the updated JSON back to the file
-    with open("AIQuestionnaire\data.json") as file:
-        json.dump(data, file)
+    jfile.append(data)
+    
+    with open("AIQuestionnaire\data.json","w") as file:
+       json.dump(jfile,file, indent =4)
+       print (jfile)
 
 
 print("Data saved as JSON.")
